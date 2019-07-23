c4164b9d47b4f45274fc08ff7a4f2cf76ed7c94e
jzhan <kolo924229628@163.com>
2019-07-23 13:29:09 +0800

    添加动态权限申请

--- study_ffmpeg/src/main/java/com/hello/study_ffmpeg/FFmpegMainActivity.java
+++ study_ffmpeg/src/main/java/com/hello/study_ffmpeg/FFmpegMainActivity.java
@@ -1,5 +1,6 @@
 package com.hello.study_ffmpeg;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.Nullable;
@@ -8,8 +9,9 @@ import android.util.Log;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.TextView;
 import android.widget.Toast;
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
 
 import java.io.File;
 
@@ -19,7 +21,9 @@ import java.io.File;
 public class FFmpegMainActivity extends AppCompatActivity {
     private SurfaceView surfaceView;
     private Player player;
+    private static final String[] needPermission = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
 
+    private static final int RC_LOCATION_CONTACTS_PERM = 124;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,13 +36,32 @@ public class FFmpegMainActivity extends AppCompatActivity {
 
     }
 
+    @AfterPermissionGranted(RC_LOCATION_CONTACTS_PERM)
     public void open(View view) {
-        File file = new File(Environment.getExternalStorageDirectory(), "DCIM/Camera/input.mp4");
-        Log.d("TAG","路径 = "+file.getAbsolutePath());
-        if (file.exists()) {
-            player.start(file.getAbsolutePath());
-        }else {
-            Toast.makeText(this,"文件不存在",Toast.LENGTH_LONG).show();
+        if (EasyPermissions.hasPermissions(this, needPermission)) {
+            File file = new File(Environment.getExternalStorageDirectory(), "DCIM/Camera/input.mp4");
+            Log.d("TAG", "路径 = " + file.getAbsolutePath());
+            if (file.exists()) {
+                player.start(file.getAbsolutePath());
+            } else {
+                Toast.makeText(this, "文件不存在", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            EasyPermissions.requestPermissions(
+                    this,
+                    "This app needs access to your location and contacts to know where and who you are.",
+                    RC_LOCATION_CONTACTS_PERM,
+                    needPermission);
         }
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // Forward results to EasyPermissions
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
     }
 }
